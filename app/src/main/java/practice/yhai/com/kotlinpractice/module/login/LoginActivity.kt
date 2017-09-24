7c48ec8ff9b770cb2fe91dc87182e560008d41bf
yanghai23 <GitHub163@126.com>
2017-09-24 16:37:18 +0800

    使用Int 扩展方法

--- app/src/main/java/practice/yhai/com/kotlinpractice/module/login/LoginActivity.kt
+++ app/src/main/java/practice/yhai/com/kotlinpractice/module/login/LoginActivity.kt
@@ -2,8 +2,10 @@ package practice.yhai.com.kotlinpractice.module.login
 
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import manage.ad.yiba.com.admanage.common.extension.toColor
 import org.jetbrains.anko.sdk25.coroutines.onClick
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.textColor
 import practice.yhai.com.kotlinpractice.R
 import practice.yhai.com.kotlinpractice.common.BaseActivity
 import practice.yhai.com.kotlinpractice.module.main.MainActivity
@@ -16,6 +18,7 @@ class LoginActivity : BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        SignInBtn.textColor = R.color.colorAccent.toColor(this)
         initListener()
     }
 
